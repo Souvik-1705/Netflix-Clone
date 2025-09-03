46794d5c94d2993f8f5952e0fa6412ad87501d62
Souvik-1705 <souvik.techdev1705@gmail.com>
2025-09-03 15:31:21 +0530

    solved login problem

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { setDoc, collection, getFirestore } from "firebase/firestore";
 import { toast } from "react-toastify";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -25,7 +25,7 @@ const signup=async(name,email,password)=>{
     try {
         const res=await createUserWithEmailAndPassword(auth,email,password);
         const user=res.user;
-        await addDoc(collection(db,"user"),{
+        await setDoc(collection(db,"user"),{
             uid:user.uid,
             name,
             authProvider:"local",
@@ -39,7 +39,8 @@ const signup=async(name,email,password)=>{
 
 const login=async(email,password)=>{
     try {
-        signInWithEmailAndPassword(auth,email,password);
+        const res = await signInWithEmailAndPassword(auth, email, password);
+        return res.user;  
     } catch (error) {
         console.log(error);
         toast.error(error.code.split('/')[1].split('-').join(" "));
